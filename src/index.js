661b7e5bbc1c7a62faf3f49e70ca47fc90303547
Alexander Kachkaev <alexander@kachkaev.ru>
2017-08-19 08:42:20 +0100

    Add old version for comparison

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { tree } from 'd3-state-visualizer';
+import oldTree from './lib-before-fix/charts/tree/tree';
 import update from 'immutability-helper';
 
-const appState = {
+const defaultAppState = {
+  test: [1],
   todoStore: {
     todos: [
       { title: 'd3' },
@@ -10,39 +12,51 @@ const appState = {
       { title: 'tree' },
     ],
   },
-  test: 1,
 };
 
-const appStates = [appState];
+const appStates = [defaultAppState];
 appStates.push(
-  update(appState, { todoStore: { completedCount: {$set: 42 } } })
-);
-appStates.push(appState);
-appStates.push(
-  update(appState, { todoStore: { todos: { $unshift: [{ title: 'hey' }] } } })
+  update(defaultAppState, { todoStore: { completedCount: {$set: 42 } } })
 );
 appStates.push(
-  update(appState, { todoStore: { todos: {4: {$set: [{ title: 'tada' }] }} }})
+  update(appStates[1], { todoStore: { todos: { $unshift: [{ title: 'hey' }] } } })
 );
 appStates.push(
-  update(appState, { todoStore: { $unset: ['todos']} })
+  update(appStates[2], { todoStore: { todos: {
+    0: {$set: [{ title: 'tada' }] },
+    2: {$set: [{ title: 'tada' }] },
+    4: {$set: [{ title: 'tada' }] }
+  } }})
+);
+appStates.push(
+  update(appStates[3], { test: {$push: [2]}} )
+  // update(appState, { $unset: ['todoStore']})
+);
+appStates.push(
+  update(defaultAppState, { todoStore: { $unset: ['todos']} })
   // update(appState, { $unset: ['todoStore']})
 );
 
-const render = tree(document.getElementById('root'), {
-  state: appState,
+const config = {
+  state: defaultAppState,
   id: 'treeExample',
-  size: 1000,
+  size: 600,
   aspectRatio: 0.5,
   isSorted: false,
   widthBetweenNodesCoeff: 1.5,
   heightBetweenNodesCoeff: 2,
   style: { border: '1px solid black' },
   tooltipOptions: { offset: { left: 30, top: 10 }, indentationSize: 2 },
-});
+  transitionDuration: 700
+};
+const stateDuration = 1000
+
+const render = tree(document.getElementById('root'), config);
+const oldRender = oldTree(document.getElementById('oldRoot'), config);
 
 let i = 0;
 setInterval(() => {
   i = (i + 1) % appStates.length;
   render(appStates[i]);
-}, 1000);
+  oldRender(appStates[i]);
+}, stateDuration);
